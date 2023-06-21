992a999d1ffda02aa8d285c93954fa6a6b16e12a
Đinh Bách Thông <thongdb.dev@gmail.com>
2023-06-21 22:08:28 +0700

    #1: Setup Server Source Code

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const morgan = require('morgan');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const getDatabase = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return process.env.DATABASE_DEV.replace(
+        '<PASSWORD>',
+        process.env.DATABASE_PASSWORD
+      );
+    case 'production':
+      return process.env.DATABASE_DEV.replace(
+        '<PASSWORD>',
+        process.env.DATABASE_PASSWORD
+      );
+    case 'local':
+      return process.env.DATABASE_LOCAL.replace(
+        '<PASSWORD>',
+        process.env.DATABASE_PASSWORD
+      );
+    default:
+      return process.env.DATABASE_LOCAL.replace(
+        '<PASSWORD>',
+        process.env.DATABASE_PASSWORD
+      );
+  }
+};
+
+const DB = getDatabase();
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.info('DB connection successfully');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.info(`App running on port ${PORT}...`);
+});
